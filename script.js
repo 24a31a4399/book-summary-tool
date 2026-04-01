1a5b668b8e330a64a812e3632595ffe9f7170bcd
24a31a4399 <hemalathavennela19@gmail.com>
2026-04-01 11:31:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 function getSummary() {
     let book = document.getElementById("book").value.toLowerCase().trim();
+    let author = document.getElementById("author").value.toLowerCase().trim();
 
     let summary = "";
 
-    if (book === "wings of fire") {
-        summary = "This book is about APJ Abdul Kalam's journey.";
+    if (book.includes("wings of fire") && author.includes("kalam")) {
+        summary = "This book is about APJ Abdul Kalam's journey from a small boy to India's Missile Man.";
     } 
-    else if (book === "rich dad poor dad") {
-        summary = "This book teaches financial knowledge.";
+    else if (book.includes("rich dad poor dad") && author.includes("robert")) {
+        summary = "This book teaches financial knowledge and importance of money management.";
     }
-    else if (book === "amma diary lo konni pageelu") {
-        summary = "This book is about emotions and life experiences written in a diary format.";
+    else if (book.includes("amma diary lo konni pageelu") && author.includes("ravi")) {
+        summary = "This book is about emotional life experiences written in a diary format by Ravi Manthri.";
     }
     else {
         summary = "Summary not available. Try another book.";
