fd4380ecbbf9bc6c0b716c9905a9591d9f6be266
24a31a4399 <hemalathavennela19@gmail.com>
2026-04-01 11:25:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 function getSummary() {
-    let book = document.getElementById("book").value.toLowerCase();
+    let book = document.getElementById("book").value.toLowerCase().trim();
 
     let summary = "";
 
     if (book === "wings of fire") {
-    summary = "This book is about APJ Abdul Kalam's journey.";
-} 
-else if (book === "rich dad poor dad") {
-    summary = "This book teaches financial knowledge.";
-}
-else if (book === "amma dairy lo konni pageelu") {
-    summary = "This book is about emotions and life experiences written in a diary format.";
-}
-else {
-    summary = "Summary not available. Try another book.";
-}
+        summary = "This book is about APJ Abdul Kalam's journey.";
+    } 
+    else if (book === "rich dad poor dad") {
+        summary = "This book teaches financial knowledge.";
+    }
+    else if (book === "amma diary lo konni pageelu") {
+        summary = "This book is about emotions and life experiences written in a diary format.";
+    }
+    else {
+        summary = "Summary not available. Try another book.";
+    }
+
     document.getElementById("output").innerText = summary;
 }
